23fcb67a7bd6f772b355a7a835d6581c7cf937ec
Brendan <hombm@dukes.jmu.edu>
2024-12-02 10:52:22 -0500

    Add comments

--- approxmation_solution/cs412_max3sat_approx.py
+++ approxmation_solution/cs412_max3sat_approx.py
@@ -1,6 +1,29 @@
 """
+    name: Brendan Hom
+    Max 3-Sat Approximation Solution
 
+    n: Number of variables.
+    m: Number of clauses.
+    L: Maximum iterations of the while loop in main.
 
+    Strategy Idea: 
+        1. Randomized Approximation
+        Assign random Boolean values to each variable.
+        Each iteration computes how many clauses are satisfied under the current assignment.
+        
+        2. Repeated Attempts
+        The algorithm repeats the random assignment process up to 10.
+        It retains the "best count" of satisfied clauses across all iterations.
+        
+        3. Stopping Conditions
+        Stops if all cluases are satisfied (best_count == m) or
+        It completes L iterations without improvement.
+
+    Runtime: 
+    1. Assignment of truth values to each variable: O(n)
+    2. Checking each cluase to see if it is satisfied: O(m)
+    
+    Time Complexity: O(n + m)
 """
 import random
 
@@ -27,7 +50,6 @@ def main():
 
     while (num_loops < 10):
         current_count, assignments = max_3sat_approx(n, clauses)
-        print(current_count)
         if current_count > best_count:
             best_count = current_count
             num_loops = 0
@@ -35,8 +57,7 @@ def main():
             break
         num_loops += 1
 
-    # Output the number of satisfied clauses
-    print()
+    # Print Best count
     print(best_count)
     # Output the variable assignments
     for var in range(1, n + 1):
