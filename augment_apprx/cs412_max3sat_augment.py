5446b2ec98742f8df1d571979b3458644d83460d
Gillian Kelly <kellygm@dukes.jmu.edu>
2024-12-06 08:37:12 -0500

    finishing touches

--- augment_apprx/cs412_max3sat_augment.py
+++ augment_apprx/cs412_max3sat_augment.py
@@ -22,30 +22,7 @@ def calculate_satisfied_clauses(clauses, assignments):
             (literal < 0 and not assignments[abs(literal)])):
                 satisfied_count += 1
                 break
-    return satisfied_count
-
-
-"""Exponential version of Max Independent Set. This was used to validate my approximation solutions"""
-def max_ind_set(graph):
-    if len(graph) == 0:
-        return []
-    
-    # select a random vertex from the graph to start
-    # v = random.choice(list(graph.keys()))
-    # select the first vertex from the graph to start
-    v = next(iter(graph))
-
-    # current vertex NOT selected -- make graph without v
-    without_v_graph = {k: neighbors - {v} for k, neighbors in graph.items() if k != v}
-
-    without_v = max_ind_set(without_v_graph)
-    
-    # Current vertex selected - make a new graph without v and its neighbors
-    with_v_graph = {k: neighbors - graph[v] for k, neighbors in graph.items() if k != v and k not in graph[v]}
-
-    with_v = [v] + max_ind_set(with_v_graph)
-
-    return max(with_v, without_v, key=len)    
+    return satisfied_count  
 
 
 """Approximation of Max Independent Set that utilized randomization"""
@@ -71,62 +48,35 @@ def max_ind_set_random(graph):
 
     return ind_set
 
-""" attempts to add non-conflicting vertices to the MIS and updates solution if it leads to a
-    more optimal result """
-def mis_local_search(graph, mis, clauses, assignments):
-    curr_mis = mis[:]
-    improvement = True
-    while improvement:
-        improvement = False
-        for v in list(graph.keys()):
-            if v not in curr_mis and all(u not in curr_mis for u in graph[v]):
-                improved_mis = curr_mis + [v]
-                improved_assgn = update_assignments(improved_mis, assignments.copy())
-                if calculate_satisfied_clauses(clauses, improved_assgn) > calculate_satisfied_clauses(clauses, assignments):
-                    assignments = improved_assgn
-                    curr_mis = improved_mis
-                    improvement = True
-                    break
-
-    return curr_mis
-
 
 """Updates the variables that are in the independent set"""
 def update_assignments(maxIndSet, assignments):
     for (i, var) in maxIndSet:
-        if var > 0:
-            assignments[abs(var)] = True
-        else:
-            assignments[abs(var)] = False
+        assignments[abs(var)] = var > 0
     return assignments
 
 
 """ Builds the adjacency list, moved to separate function for cleanliness"""
 def build_graph(clauses):
-    # read in the next m clauses, and built adj_list with 
-    # unqiue vertex name that includes the clause it corresponds to
-    adj_list = {}
+    adj_list = collections.defaultdict(set)
+    clause_map = collections.defaultdict(list)
+
     for i, (x1, x2, x3) in enumerate(clauses):
-        # vertices represented as tuples with (clause #, variable)
         vertices = [(i, x) for x in (x1, x2, x3)]
         for j, v1 in enumerate(vertices):
-            if v1 not in adj_list:
-                adj_list[v1] = set()
-            for k in range(3):
-                if j != k:
-                    v2 = vertices[k]
-                    adj_list[v1].add(v2)
+            adj_list[v1].update(vertices[:j] + vertices[j+1:])
 
-    # add edges between contradicting variables across clauses
-    for i, (x1, x2, x3) in enumerate(clauses):
+        # Map literals to their clause indices
         for x in (x1, x2, x3):
-            opp = (i, -x)
-            # check if opp in any other clauses
-            for j, (y1, y2, y3) in enumerate(clauses):
-                if i != j and -x in (y1, y2, y3):
+            clause_map[x].append(i)
+
+    # Add contradicting edges
+    for x, clause_indices in clause_map.items():
+        for i in clause_indices:
+            for j in clause_map[-x]:
+                if i != j:
                     adj_list[(i, x)].add((j, -x))
                     adj_list[(j, -x)].add((i, x))
-
     return adj_list
 
 
@@ -140,15 +90,22 @@ def main():
     adj_list = build_graph(clauses)
 
     assignments = {var: False for var in range(1, n + 1)}
+    
+    i = 0
+    best_results = 0
+    while i < 5:
+        mis = max_ind_set_random(adj_list)
 
-    mis = max_ind_set_random(adj_list)
-
-    refined_mis = mis_local_search(adj_list, mis, clauses, assignments)
-    update_assignments(refined_mis, assignments)
-    satisifed_clauses = calculate_satisfied_clauses(clauses, assignments)
+        # refined_mis = local_search_mis(adj_list, mis, clauses, assignments)
+        update_assignments(mis, assignments)
+        satisifed_clauses = calculate_satisfied_clauses(clauses, assignments)
+        if satisifed_clauses > best_results:
+            best_results = satisifed_clauses
+        
+        i += 1
 
     # print num of satisfied clauses
-    formatted = ", ".join(map(str, refined_mis))
+    formatted = ", ".join(map(str, mis))
     print(f"{satisifed_clauses}")
 
     # print variables with their assignment
--- augment_apprx/test_cases/run_tests.sh
+++ augment_apprx/test_cases/run_tests.sh
@@ -10,25 +10,25 @@ BLUE="\033[0;34m"
 UL="\e[430m"
 
 echo -e "${BOLD}Test cases:"
-echo -e "\t${BOLD}test\t\taugment\t\truntime\t\tapprox${NC}"
+echo -e "\t${BOLD}test\t\taugment\t\texact${NC}"
 
 
 PROG_TO_TEST=../cs412_max3sat_augment.py
 
-for test in test*
+for test in $(find . -name "test*" -type d | sort -V);
 do
     cd $test
-    start=`python3 -c 'import time; print(time.time())'`
-    python3 ../${PROG_TO_TEST} < input.txt > output.txt
-    end=`python3 -c 'import time; print(time.time())'`
-    runtime=$( echo "$end - $start" | bc -l )
+    start=`python -c 'import time; print(time.time())'`
+    python ../${PROG_TO_TEST} < input.txt > output.txt
+    end=`python -c 'import time; print(time.time())'`
+    runtime=$( echo "$end - $start" )
 
 
     if [ "$(head -n 1 expected.txt)" = "$(head -n 1 output.txt)" ]
     then
-        echo -e "\t${test}\t\t$(head -n 1 output.txt)\t\t${BLUE}${runtime}s${NC}\t\t$(head -n 1 expected.txt)"
+        echo -e "\t${test}\t\t$(head -n 1 output.txt)${NC}\t\t$(head -n 1 expected.txt)"
     else
-        echo -e "\t${test}\t\t$(head -n 1 output.txt)\t\t${BLUE}${runtime}s${NC}\t\t$(head -n 1 expected.txt)"
+        echo -e "\t${test}\t\t$(head -n 1 output.txt)${NC}\t\t$(head -n 1 expected.txt)"
     fi
 
     cd ../
